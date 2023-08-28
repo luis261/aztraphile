870d5c0e91611c3f1c2cc445238cb3279012bcf7
Goerler <Luis.Goerler@wacker.com>
2023-08-28 06:19:35 +0200

    Remove duplicated tests

--- test-samples/http_default_function_sample_test.py
+++ test-samples/http_default_function_sample_test.py
@@ -4,40 +4,6 @@ import json
 import pytest
 
 
-def test_http_default_sample_no_name():
-    req = func.HttpRequest(method="GET", url="/api/http_function_sample", body=json.dumps({}).encode("utf8"))
-    main_func = main.build().get_user_function()
-
-    resp = main_func(req)
-
-    assert resp.status_code == 200
-    assert "executed successfully" in resp.get_body().decode("utf-8")
-
-def test_http_default_sample_query_name():
-    req = func.HttpRequest(method="GET",
-        url="/api/http_function_sample",
-        params={"name": "Bob"}, body=json.dumps({}).encode("utf8"))
-    main_func = main.build().get_user_function()
-
-    resp = main_func(req)
-
-    assert resp.status_code == 200
-    assert "Hello, Bob" in resp.get_body().decode("utf-8")
-
-def test_http_default_sample_body_name():
-    req = func.HttpRequest(method="GET",
-        url="/api/http_function_sample",
-        body=json.dumps({
-            "name": "Bob"
-        }).encode("utf8"))
-    main_func = main.build().get_user_function()
-
-    resp = main_func(req)
-
-    assert resp.status_code == 200
-    assert "Hello, Bob" in resp.get_body().decode("utf-8")
-
-# TODO see if the output is hopefully readable on failure, if so, delete other test cases
 @pytest.mark.parametrize(
     "query_params, input_body, status_code, returned_body_keyword",
     [
