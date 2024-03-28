e4c82e3db1c989f81157b3e92507b30851655cbf
Luis <72552948+luis261@users.noreply.github.com>
2024-03-28 12:08:55 +0100

    Persist initial config

--- create-py-function-app.ps1
+++ create-py-function-app.ps1
@@ -377,7 +377,11 @@ try {
     Assert-GitStatusOk
     git checkout -b main *>>"$GitLogFile"
     Assert-GitStatusOk
-    Add-Content -Path 'README.md' -Value "![Aztraphile Badge](https://img.shields.io/badge/setup_by_aztraphile-000000?style=flat&logo=powershell&logoColor=blue&labelColor=white&color=012456)"
+    Add-Content -Path 'README.md' -Value '![Aztraphile Badge](https://img.shields.io/badge/setup_by_aztraphile-000000?style=flat&logo=powershell&logoColor=blue&labelColor=white&color=012456)'
+    Add-Content -Path 'README.md' -Value "`nThis is the initial config this repo was generated with:"
+    Add-Content -Path 'README.md' -Value '```'
+    Get-Content "$PSScriptRoot/$ConfigFile" | Add-Content -Path 'README.md'
+    Add-Content -Path 'README.md' -Value '```'
     Add-ToIndex 'README.md'
     git commit -m 'Initial commit: add generated README' *>>"$GitLogFile"
     Assert-GitStatusOk
